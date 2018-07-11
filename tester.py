5a75138f6b41b9627fc8a04efd92e38779e4d723
flavioeverardo <flavio.everardo@cs.uni-potsdam.de>
2018-07-11 03:19:15 +0200

    more tests added for remove_zeros, check_sat and no_gje

--- tester.py
+++ tester.py
@@ -10,15 +10,21 @@ class TestCase(unittest.TestCase):
             if sys.version_info[0] < 3
             else unittest.TestCase.assertRaisesRegex(self, *args, **kwargs))
 
-def solve(m, show):
-    matrix = []
-
+def check_sat(m):
+    ## Check SAT
+    return gje.check_sat(m)
+    
+def remove_rows_zeros(m):
+    ## Remove rows with all zeros including the augmented column
+    matrix = gje.remove_rows_zeros(m)
+    return matrix
+    
+def solve_gje(m, show):
     ## If there are more than unary xors perform GJE
     if len(m[0]) > 2:
-        matrix = gje.remove_rows_zeros(m)
-        matrix = gje.perform_gauss_jordan_elimination(matrix, show)
-    
-    return matrix
+        m = gje.remove_rows_zeros(m)
+        m = gje.perform_gauss_jordan_elimination(m, show)
+    return m
 
 
 class TestProgramTransformer(TestCase):
@@ -27,29 +33,49 @@ class TestProgramTransformer(TestCase):
     If True, it will display the GJ Elimination Procedure
     """
 
-    ## More Columns
+    """ 
+    Gauss-Jorda Elimination Tests
+    """
+
+    ## No GJE due matrix size. Return the same matrix to check SAT
+    def test_no_gje_one(self):
+        self.assertEqual(solve_gje([[1, 0],
+                                    [1, 1],
+                                    [1, 0]],False),
+                         [[1, 0],
+                          [1, 1],
+                          [1, 0]])
+
+    def test_no_gje_two(self):
+        self.assertEqual(solve_gje([[1, 0],
+                                    [0, 1]],False),
+                         [[1, 0],
+                          [0, 1]])
+        
+    
+    ## More Columns than Rows
     def test_more_cols_one(self):
-        self.assertEqual(solve([[0, 1, 1, 0, 0],
-                                [0, 1, 1, 0, 0],
-                                [1, 0, 0, 1, 0]],False),
+        self.assertEqual(solve_gje([[0, 1, 1, 0, 0],
+                                    [0, 1, 1, 0, 0],
+                                    [1, 0, 0, 1, 0]],False),
                          [[1, 0, 0, 1, 0],
                           [0, 1, 1, 0, 0],
                           [0, 0, 0, 0, 0]])
 
     def test_more_cols_two(self):
-        self.assertEqual(solve([[0, 1, 1, 0],
-                                [0, 1, 1, 0],
-                                [1, 0, 0, 0]],False),
+        self.assertEqual(solve_gje([[0, 1, 1, 0],
+                                    [0, 1, 1, 0],
+                                    [1, 0, 0, 0]],False),
                          [[1, 0, 0, 0],
                           [0, 1, 1, 0],
                           [0, 0, 0, 0]])
 
     def test_more_cols_three(self):
-        self.assertEqual(solve([[1, 0, 1, 0, 0, 0, 0, 0],
-                                [1, 1, 1, 0, 0, 0, 0, 1],
-                                [0, 0, 0, 1, 1, 0, 0, 1],
-                                [0, 0, 0, 0, 0, 1, 1, 0],
-                                [0, 0, 0, 1, 0, 0, 0, 0]],False),
+        self.assertEqual(solve_gje([[1, 0, 1, 0, 0, 0, 0, 0],
+                                    [1, 1, 1, 0, 0, 0, 0, 1],
+                                    [0, 0, 0, 1, 1, 0, 0, 1],
+                                    [0, 0, 0, 0, 0, 1, 1, 0],
+                                    [0, 0, 0, 1, 0, 0, 0, 0]],False),
                          [[1, 0, 1, 0, 0, 0, 0, 0],
                           [0, 1, 0, 0, 0, 0, 0, 1],
                           [0, 0, 0, 1, 0, 0, 0, 0],
@@ -57,12 +83,12 @@ class TestProgramTransformer(TestCase):
                           [0, 0, 0, 0, 0, 1, 1, 0]])
 
     def test_more_cols_four(self):
-        self.assertEqual(solve([[0, 1, 0, 0, 0, 0, 0, 1],
-                                [0, 1, 1, 0, 0, 0, 0, 0],
-                                [0, 0, 0, 1, 1, 0, 0, 1],
-                                [0, 0, 0, 0, 0, 1, 1, 0],
-                                [0, 1, 0, 0, 0, 0, 1, 0],
-                                [1, 0, 0, 1, 0, 0, 0, 0]],False),
+        self.assertEqual(solve_gje([[0, 1, 0, 0, 0, 0, 0, 1],
+                                    [0, 1, 1, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 1, 1, 0, 0, 1],
+                                    [0, 0, 0, 0, 0, 1, 1, 0],
+                                    [0, 1, 0, 0, 0, 0, 1, 0],
+                                    [1, 0, 0, 1, 0, 0, 0, 0]],False),
                          [[1, 0, 0, 0, 1, 0, 0, 1],
                           [0, 1, 0, 0, 0, 0, 0, 1],
                           [0, 0, 1, 0, 0, 0, 0, 1],
@@ -71,24 +97,24 @@ class TestProgramTransformer(TestCase):
                           [0, 0, 0, 0, 0, 0, 1, 1]])
 
     def test_more_cols_five(self):
-        self.assertEqual(solve([[1, 0, 1, 0, 1, 1, 0, 0],
-                                [1, 1, 1, 0, 0, 0, 1, 1],
-                                [0, 0, 1, 0, 1, 0, 0, 1],
-                                [0, 1, 0, 1, 0, 1, 1, 0],
-                                [0, 0, 0, 1, 0, 0, 0, 0]],False),
+        self.assertEqual(solve_gje([[1, 0, 1, 0, 1, 1, 0, 0],
+                                    [1, 1, 1, 0, 0, 0, 1, 1],
+                                    [0, 0, 1, 0, 1, 0, 0, 1],
+                                    [0, 1, 0, 1, 0, 1, 1, 0],
+                                    [0, 0, 0, 1, 0, 0, 0, 0]],False),
                          [[1, 0, 0, 0, 0, 1, 0, 1],
                           [0, 1, 0, 0, 0, 1, 1, 0],
                           [0, 0, 1, 0, 0, 0, 0, 0],
                           [0, 0, 0, 1, 0, 0, 0, 0],
                           [0, 0, 0, 0, 1, 0, 0, 1]])
 
-    ## Square
+    ## Square Matrix
     def test_square_one(self):
-        self.assertEqual(solve([[1, 0, 1, 0, 1, 0],
-                                [1, 1, 1, 0, 0, 1],
-                                [0, 0, 1, 0, 1, 1],
-                                [0, 1, 0, 1, 0, 0],
-                                [0, 0, 0, 1, 0, 0]],False),
+        self.assertEqual(solve_gje([[1, 0, 1, 0, 1, 0],
+                                    [1, 1, 1, 0, 0, 1],
+                                    [0, 0, 1, 0, 1, 1],
+                                    [0, 1, 0, 1, 0, 0],
+                                    [0, 0, 0, 1, 0, 0]],False),
                          [[1, 0, 0, 0, 0, 1],
                           [0, 1, 0, 0, 0, 0],
                           [0, 0, 1, 0, 0, 0],
@@ -96,29 +122,29 @@ class TestProgramTransformer(TestCase):
                           [0, 0, 0, 0, 1, 1]])
 
     def test_square_two(self):
-        self.assertEqual(solve([[1, 0, 1, 1, 1],
-                                [1, 0, 1, 0, 0],
-                                [0, 1, 0, 0, 1],
-                                [0, 0, 1, 1, 0]],False),
+        self.assertEqual(solve_gje([[1, 0, 1, 1, 1],
+                                    [1, 0, 1, 0, 0],
+                                    [0, 1, 0, 0, 1],
+                                    [0, 0, 1, 1, 0]],False),
                          [[1, 0, 0, 0, 1],
                           [0, 1, 0, 0, 1],
                           [0, 0, 1, 0, 1],
                           [0, 0, 0, 1, 1]])
 
     def test_square_three(self):
-        self.assertEqual(solve([[1, 1, 1, 1],
-                                [1, 0, 1, 0],
-                                [0, 0, 1, 0]],False),
+        self.assertEqual(solve_gje([[1, 1, 1, 1],
+                                    [1, 0, 1, 0],
+                                    [0, 0, 1, 0]],False),
                          [[1, 0, 0, 0],
                           [0, 1, 0, 1],
                           [0, 0, 1, 0]])
 
     def test_square_four(self):
-        self.assertEqual(solve([[0, 0, 1, 1, 1, 0],
-                                [0, 1, 1, 1, 0, 1],
-                                [1, 0, 1, 1, 1, 1],
-                                [0, 1, 0, 1, 0, 0],
-                                [1, 0, 0, 1, 0, 1]],False),
+        self.assertEqual(solve_gje([[0, 0, 1, 1, 1, 0],
+                                    [0, 1, 1, 1, 0, 1],
+                                    [1, 0, 1, 1, 1, 1],
+                                    [0, 1, 0, 1, 0, 0],
+                                    [1, 0, 0, 1, 0, 1]],False),
                          [[1, 0, 0, 0, 0, 1],
                           [0, 1, 0, 0, 0, 0],
                           [0, 0, 1, 0, 0, 1],
@@ -126,19 +152,19 @@ class TestProgramTransformer(TestCase):
                           [0, 0, 0, 0, 1, 1]])
 
     def test_square_five(self):
-        self.assertEqual(solve([[1, 1, 1],
-                                [1, 0, 1]],False),
+        self.assertEqual(solve_gje([[1, 1, 1],
+                                    [1, 0, 1]],False),
                          [[1, 0, 1],
                           [0, 1, 0]])
 
 
-    ## More Rows
+    ## More Rows than Columns
     def test_more_rows_one(self):
-        self.assertEqual(solve([[1, 0, 1, 0],
-                                [1, 1, 1, 0],
-                                [0, 1, 0, 1],
-                                [0, 0, 1, 0],
-                                [0, 1, 0, 1]],False),
+        self.assertEqual(solve_gje([[1, 0, 1, 0],
+                                    [1, 1, 1, 0],
+                                    [0, 1, 0, 1],
+                                    [0, 0, 1, 0],
+                                    [0, 1, 0, 1]],False),
                          [[1, 0, 0, 0],
                           [0, 1, 0, 0],
                           [0, 0, 1, 0],
@@ -146,22 +172,83 @@ class TestProgramTransformer(TestCase):
                           [0, 0, 0, 1]])
 
     def test_more_rows_two(self):
-        self.assertEqual(solve([[0, 1, 0],
-                                [0, 1, 1],
-                                [1, 0, 0],
-                                [1, 1, 0]],False),
+        self.assertEqual(solve_gje([[0, 1, 0],
+                                    [0, 1, 1],
+                                    [1, 0, 0],
+                                    [1, 1, 0]],False),
                          [[1, 0, 0],
                           [0, 1, 1],
                           [0, 0, 1],
                           [0, 0, 1]])
 
     def test_more_rows_three(self):
-        self.assertEqual(solve([[0, 1, 1],
-                                [1, 0, 0],
-                                [0, 0, 0]],False),
+        self.assertEqual(solve_gje([[0, 1, 1],
+                                    [1, 0, 0],
+                                    [0, 0, 0]],False),
                          [[1, 0, 0],
                           [0, 1, 1]])
-        
+
+
+    """ 
+    Pre GJE... Remove Rows if they are all zeros
+    """
+    ## Remove Rows full of Zeros 
+    def test_remove_zeros_one(self):
+        self.assertEqual(remove_rows_zeros([[1, 0, 1, 0],
+                                            [1, 1, 1, 0],
+                                            [0, 1, 0, 1],
+                                            [0, 0, 0, 0],
+                                            [0, 0, 0, 0]]),
+                         [[1, 0, 1, 0],
+                          [1, 1, 1, 0],
+                          [0, 1, 0, 1]])
+
+    def test_remove_zeros_two(self):
+        self.assertEqual(remove_rows_zeros([[1, 0, 0],
+                                            [0, 1, 1],
+                                            [0, 0, 1],
+                                            [0, 0, 0]]),
+                         [[1, 0, 0],
+                          [0, 1, 1],
+                          [0, 0, 1]])
+
+    def test_remove_zeros_three(self):
+        self.assertEqual(remove_rows_zeros([[0, 1, 1],
+                                            [1, 0, 0],
+                                            [0, 0, 0]]),
+                         [[0, 1, 1],
+                          [1, 0, 0]])
+
+
+    """ 
+    Check Satisfiability wrt the augmented column. 
+    It must exist an empty odd equation
+    """
+    ## Check SATISFIABILITY
+    def test_check_sat_one(self):
+        self.assertEqual(check_sat([[1, 0, 1, 0],
+                                     [1, 1, 1, 0],
+                                     [0, 1, 0, 1],
+                                     [0, 0, 0, 1],
+                                     [0, 0, 0, 0]]),True)
+
+    def test_check_sat_two(self):
+        self.assertEqual(check_sat([[1, 0, 0],
+                                    [0, 1, 1],
+                                    [1, 0, 1],
+                                    [1, 1, 0]]),False)
+
+    def test_check_sat_three(self):
+        self.assertEqual(check_sat([[1, 0, 1],
+                                    [0, 1, 0],
+                                    [0, 0, 1]]),True)
+
+    def test_check_sat_four(self):
+        self.assertEqual(check_sat([[1, 0, 0, 0, 0, 0],
+                                    [0, 1, 0, 0, 0, 1],
+                                    [0, 0, 1, 0, 0, 1],
+                                    [0, 0, 0, 1, 0, 0],
+                                    [0, 0, 0, 0, 1, 1]]),False)
         
 
 if __name__ == '__main__':
